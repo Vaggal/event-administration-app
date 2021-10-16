992ed1608784621dce37f644a50aa5d4fbe9f991
Vangelis Galanis <vaggalanis@gmail.com>
2021-10-16 20:42:01 +0300

    build: install mongoose and establish connection with the database

---
+++ server/db.ts
@@ -0,0 +1,17 @@
+import { connect } from 'mongoose';
+
+const user = process.env.MONGO_USER || 'test';
+const password = process.env.MONGO_PASS || 'testpass';
+const mongoPort = process.env.MONGO_PORT || 27017;
+
+const mongoHost = process.env.MONGO_HOST || 'localhost';
+const auth = user ? `${user}:${password}@` : '';
+const DB_STRING = `mongodb://${auth}${mongoHost}:${mongoPort}/eventsDB`;
+
+const config = { useNewUrlParser: true, useUnifiedTopology: true, authSource: 'eventsDB' };
+
+const connectToDB = (): Promise<typeof import('mongoose')> => {
+  return connect(DB_STRING, config);
+};
+
+export { DB_STRING, connectToDB };
