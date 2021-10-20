23b57e87783ba58e8102b42874de472967a1382a
Vangelis Galanis <vaggalanis@gmail.com>
2021-10-20 19:03:42 +0300

    feat: add more fields in the event model and transform the output of the response

--- mongo-init.js
+++ mongo-init.js
@@ -1,17 +1,26 @@
 let res = [
   db.events.drop(),
   db.events.insert({
+    organizer: "Organizer 1",
     title: "Concert 1",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     date: new Date("2021-10-21T16:00:00Z"),
     location: { latitude: 51.5073219, longitude: -0.1276474 },
   }),
   db.events.insert({
+    organizer: "Organizer 2",
     title: "Concert 2",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     date: new Date("2021-10-25T21:00:00Z"),
     location: { latitude: 52.5170365, longitude: 13.3888599 },
   }),
   db.events.insert({
+    organizer: "Organizer 3",
     title: "Concert 3",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
     date: new Date("2021-10-28T18:00:00Z"),
     location: { latitude: 40.4167047, longitude: -3.7035825 },
   }),
--- server/models/event.ts
+++ server/models/event.ts
@@ -1,13 +1,26 @@
 import { Schema, model } from 'mongoose';
 
-const eventSchema = new Schema({
-  title: String,
-  date: Date,
-  location: {
-    longitude: Number,
-    latitude: Number,
+const eventSchema = new Schema(
+  {
+    organizer: String,
+    title: String,
+    description: String,
+    date: Date,
+    location: {
+      longitude: Number,
+      latitude: Number,
+    },
   },
-});
+  {
+    toJSON: {
+      transform: (doc, ret) => {
+        ret.id = ret._id;
+        delete ret.__v;
+        delete ret._id;
+      },
+    },
+  }
+);
 const eventModel = model('Event', eventSchema);
 
 export default eventModel;
