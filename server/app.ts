43c534bc3674e6648d4de7d0d4104c0ac17477a7
Vangelis Galanis <vaggalanis@gmail.com>
2021-10-22 02:25:35 +0300

    build: switch to ng serve for dev script and start using cors package in express to support this

--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,7 @@
 import * as express from 'express';
 import * as path from 'path';
 import { Express } from 'express';
+import * as cors from 'cors';
 
 import eventsRouter from './routes/events';
 
@@ -11,6 +12,7 @@ export default function createApp(): Express {
 
   const app = express();
   app.use(express.json());
+  app.use(cors());
 
   const clientDir = path.join(__dirname, '../public');
   app.use(express.static(clientDir));
