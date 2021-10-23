2b1a4e99ef8310dac09e1ca07072f9084432266b
Vangelis Galanis <vaggalanis@gmail.com>
2021-10-23 20:13:44 +0300

    test: make the test to request the api

--- server/app.spec.ts
+++ server/app.spec.ts
@@ -1,5 +1,20 @@
+import app from './app';
+import { connectToDB, disconnectFromDB } from './db';
+
+import * as request from 'supertest';
+
 describe('AppComponent', () => {
-  it('should run tests', () => {
-    expect(1 <= 2).toBeTruthy();
+  beforeEach(() => {
+    return connectToDB();
+  });
+
+  afterEach(() => {
+    return disconnectFromDB();
+  });
+
+  it('should run return the events', async () => {
+    const response = await request(app()).get('/api/events');
+    expect(response.statusCode).toBe(200);
+    expect(response.body).toBeInstanceOf(Array);
   });
 });
